a9592e141dc01aae6a76dc536cc8be4080fbaa6e
Carlos Caicedo <calicheccmusica@gmail.com>
2022-03-08 12:22:59 -0800

    oop - javascrit

---
+++ ninja.js
@@ -0,0 +1,45 @@
+// ~~~~~~ Create Ninja Object ~~~~~~~~ // 
+
+class Ninja {
+
+    // ~~~~~ Ninja Constructor ~~~~~ //
+    constructor(name, health) {
+        this.name = name;
+        this.health = health;
+        this.speed = 30;
+        this.strength = 30;
+    }
+
+    // ~~~~~ Ninja methods ~~~~~ //
+    sayName() {
+        console.log(`My Ninja name is ... ${this.name}`);
+    }
+
+    showStats() {
+        console.log(`^-^-^ Ninja's Stats ^-^-^`);
+        console.log(`Name: ${this.name}`);
+        console.log(`Strength: ${this.strength}`);
+        console.log(`Speed: ${this.speed}`);
+        console.log(`Health: ${this.health}`);
+    }
+
+    drinkSake() {
+        this.health += 10;
+    }
+
+    practiceTaijutsu() {
+        this.strength += 15;
+    }
+}
+
+// ***** Test ***** // 
+const warrior = new Ninja("Hattori Mochizuki", 150);
+console.log(warrior.name);
+console.log(warrior.health);
+console.log(warrior.speed);
+console.log(warrior.strength);
+warrior.sayName();
+warrior.showStats();
+warrior.drinkSake();
+warrior.practiceTaijutsu();
+warrior.showStats();
